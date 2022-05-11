b9f59fb6feeb20b17a2c9cdf264bc6b908959031
altamashaltu <105192701+altamashaltu@users.noreply.github.com>
2022-05-11 12:15:33 +0530

    Add files via upload
    arcoperation program

---
+++ practical/arcoperation.java
@@ -0,0 +1,25 @@
+package corejava;
+
+public class arcoperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1, num2, add, sub, mul, div, mod;
+num1=12;
+num2=3;
+
+add=num1 + num2;
+sub=num1 - num2;
+mul=num1 * num2;
+div=num1 / num2;
+mod=num1 % num2;
+
+System.out.println("addition of two numbers="+add);
+//printing results system.out.println("subtraction of two numbers="+sub);
+System.out.println("multiplication of two numbers="+mul);
+System.out.println("division of two numbers="+div);
+System.out.println("modulus of two numbers="+mod);
+
+	}
+
+}
