a28cfb3e7b9ee7f20edf0c787e54415189cbc83f
altamashaltu <105192701+altamashaltu@users.noreply.github.com>
2022-05-12 12:34:39 +0530

    Add files via upload
    narrowcasting program

---
+++ practical/narrowcasting.java
@@ -0,0 +1,21 @@
+package corejava;
+
+
+	//Java program to illustrate explicit type conversion
+	public class narrowcasting
+	{
+	public static void main(String[] args)
+	{
+	double d = 200.06;
+	//explicit type casting
+	long l = (long)d;
+	//explicit type casting
+	int i = (int)l;
+	System.out.println("Double Data type value "+d);
+	//fractional part lost
+	System.out.println("Long Data type value "+l);
+	//fractional part lost
+	System.out.println("Int Data type value "+i);
+	}
+	}
+	
