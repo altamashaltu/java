94d795af0df19792c7188e19dffcebc2f757b38f
altamashaltu <105192701+altamashaltu@users.noreply.github.com>
2022-05-19 11:53:35 +0530

    Add files via upload

---
+++ practical/Breakstatement.java
@@ -0,0 +1,15 @@
+package corejava;
+
+public class Breakstatement {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++ ) {
+			if (i == 4) {
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
