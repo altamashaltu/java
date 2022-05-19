98f333891fa5d5a159aae33567bb1bb1381a9801
altamashaltu <105192701+altamashaltu@users.noreply.github.com>
2022-05-19 11:52:04 +0530

    Add files via upload
    constructors

---
+++ practical/Defaultconstructor.java
@@ -0,0 +1,15 @@
+package corejava;
+
+class Defaultconstructor {
+	Defaultconstructor()
+	{
+		System.out.println("this is Default constructor/No Argument constructor");
+	}
+	
+	public static void main(String[] args) {
+		Defaultconstructor dc= new Defaultconstructor();
+		
+		
+	}
+
+}
