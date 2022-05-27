f5b16b2b4410103c01c5793a9968d97caf3917f1
altamashaltu <105192701+altamashaltu@users.noreply.github.com>
2022-05-27 11:07:53 +0530

    Add files via upload

---
+++ hands on/test 26-05-2022/Leftstarpattern.java
@@ -0,0 +1,27 @@
+package corejava;
+
+public class Leftstarpattern {
+
+	public static void main(String[] args) {
+
+			int n = 5;
+			int m = 1;
+			for (int i = 0; i <= 5; i++) {
+
+				for (int j = 0; j <= n - i; j++) {
+					System.out.print(" ");
+				}
+
+				for(int k=1 ; k<=m ; k++) {
+					System.out.print("*");
+				}
+				System.out.println("");
+				m=m+1;
+				
+			}
+
+			
+		}
+	}
+
+
