687e49ed75c1a62bf6b5c9c121ea83372459ed1b
altamashaltu <105192701+altamashaltu@users.noreply.github.com>
2022-05-27 11:01:26 +0530

    Add files via upload

---
+++ hands on/Fibonacciseries.java
@@ -0,0 +1,28 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class Fibonacciseries {
+	
+	public static void main(String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of Fibonacci series : ");
+		int n = sc.nextInt();
+		sc.close();
+		int n1=0, n2=1, n3;
+		
+		for(int i=2; i<n ; i++) {
+			n3 = n1 + n2;
+			n1 = n2 ;
+			n2 = n3;
+			
+			System.out.print(" "+n3);
+			
+		}
+		
+	}
+
+}
+
+
